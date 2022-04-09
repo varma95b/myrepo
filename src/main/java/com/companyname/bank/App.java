1f7d04865434ad3ce1d86e09a9faa9d6c1b8ad16
varma95b <96039640+varma95b@users.noreply.github.com>
2022-04-09 07:40:15 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is pole scm change for webhook8" );
+        System.out.println( "this is pole scm change for webhook10" );
     }
 }
