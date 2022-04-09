b66e5b2633703adac79c55c0849500a4b534e03a
varma95b <96039640+varma95b@users.noreply.github.com>
2022-04-09 07:33:43 +0530

    today first trigger 9

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is pole scm change for webhook6" );
+        System.out.println( "this is pole scm change for webhook9" );
     }
 }
